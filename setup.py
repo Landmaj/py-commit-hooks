223a1aa04217fe9b1669189647138ab7ad249647
Michał Wieluński <michal@w-ski.dev>
2018-09-09 10:54:50 +0200

    add black

--- setup.py
+++ setup.py
@@ -4,8 +4,9 @@ setup(
     name="pre_commit_dummy_package",
     version="0.1",
     install_requires = [
-        'autoflake==1.2',
-        'seed-isort-config==1.2',
-        'isort==4.3.3',
+        'autoflake',
+        'seed-isort-config',
+        'isort',
+        'black',
     ]
 )
